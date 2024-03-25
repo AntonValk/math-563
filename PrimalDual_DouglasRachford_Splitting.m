8124407ae3add273e2cdc77d0fad0c9eb97e9955
AGerkis <a.gerkis1@gmail.com>
2024-03-25 17:38:24 -0400

    Adjusted main algorithm code to have compatible matrix sizes. Did not touch resolvent code.

--- PrimalDual_DouglasRachford_Splitting.m
+++ PrimalDual_DouglasRachford_Splitting.m
@@ -1,13 +1,8 @@
-function  xSol=PrimalDual_DouglasRachford_Splitting(b, t, rho)
+function  xSol=PrimalDual_DouglasRachford_Splitting(b, kernel, t, rho)
 [numRows, numCols]=size(b);
-p = zeros(numRows, numCols);
-q1 = zeros(numRows, numCols);  % q1 is for l1/l2^2norm
-q2 = zeros(numRows, numCols);  % q2 and q3 are for isoNorm
-q3 = zeros(numRows, numCols);  
-t=1.0  %t = i.tprimaldr; % Need to change this for the various algorithms you are applying
+
 %computes the numRow x numCol matrix of the eigenvalues for K and D1 and
 %D2; Here D1 = I oplus D1 in the paper and D2 = D1 oplus I.
-kernel = fspecial('gaussian', [15,15], 5);
 eigArry_K = eigValsForPeriodicConvOp(kernel, numRows, numCols);
 eigArry_D1 = eigValsForPeriodicConvOp([-1,1]', numRows, numCols);
 eigArry_D2 = eigValsForPeriodicConvOp([-1,1], numRows, numCols);
@@ -37,34 +32,44 @@ applyDTrans = @(y) applyD1Trans(y(:,:,1)) + applyD2Trans(y(:, :, 2));
 % Function which computes the (I + K^TK + D^TD)x where x in R^(m x n)
 % matrix and the eigenvalues of I + t*t*K^TK + t*t*D^TD; here t is the
 % stepsizes
-t=1.0  %t = i.tprimaldr; % Need to change this for the various algorithms you are applying
 applyMat = @(x) x + applyKTrans(applyK(x)) + applyDTrans(applyD(x));
 eigValsMat = ones(numRows, numCols) + t*t*eigArry_KTrans.*eigArry_K + t*t*eigArry_D1Trans.*eigArry_D1...
     + t*t*eigArry_D2Trans.*eigArry_D2;
 
 %R^(m x n) Computing (I + K^T*K + D^T*D)^(-1)*x
 invertMatrix = @(x) ifft2(fft2(x)./eigValsMat);
+
+% Initialize
+pk = zeros(numRows, numCols); % n x n matrix
+qk= zeros(3*numRows, numCols);  % 3n x n matrix
+
 for k = 1:500
-    x = boxProx(p,t);   %x is n by n matrix
-    w=zeros(numRows,numCols,2);
-    w(:,:,1)=q2;   % put z22 and z23 into a 3d matrix of size nxnx2
-    w(:,:,2)=q3;
-    y1 = l1Prox(q1,t);
-    y2 = isoProx(w,t);
-    y = [y1;y2(:,:,1);y2(:,:,2)];   %y is 3n by n matrix
-    z = [q1;q2;q3]-y;   %z is 3n by n matrix
-    inverse = invertMatrix(eye(numRows^2));
-    iTA = [eye(numRows^2);t*applyK(eye(numRows^2));t*applyD1(eye(numRows^2));t*applyD2(eye(numRows^2))];
-    iTATrans = [eye(numRows^2),-t*applyKTrans(eye(numRows^2)),-t*applyD1Trans(eye(numRows^2));-t*applyD2Trans(eye(numRows^2))];
+    % Extract elements
+    q1 = qk(1:numRows, :);
+    q2 = cat(3, qk((numRows + 1):2*numRows, :), qk((2*numRows + 1):end, :)); % Change q from 2D (2n*n)->3D (n*n*2)
+
+    % Compute Prox Ops
+    xk = boxProx(pk,t);   %x is n by n matrix
+    z1 = l1Prox(q1 - b, t) + b; % Equation 8 in reference
+    z2 = isoProx(q2, t);
+    zk = [q1; q2(:, :, 1); q2(:, :, 2)] - [z1; z2(:, :, 1); z2(:, :, 2)];
+
+    % Compute Resolvent of B
+    inverse = invertMatrix(eye(numRows));
+    iTA = [eye(numRows);t*applyK(eye(numRows));t*applyD1(eye(numRows));t*applyD2(eye(numRows))];
+    iTATrans = [eye(numRows),-t*applyKTrans(eye(numRows)),-t*applyD1Trans(eye(numRows));-t*applyD2Trans(eye(numRows))];
     bigProd = iTA.*inverse.*iTATrans;
-    blockWithId = [zeros(2*numRows^2),zeros(2*numRows^2);zeros(2*numRows^2),eye(2*numRows^2)];
+    blockWithId = [zeros(2*numRows),zeros(2*numRows);zeros(2*numRows),eye(2*numRows)];
     bigSum = blockWithId+bigProd;
-    aux = [2*x-p;2*z-[q1;q2:q3]];
+    aux = [2*xk - pk; 2*zk - qk];
     res = bigSum .* aux(:);
-    w = reshape(res(1:numRos^2,:),numRows,numCols);
-    v = reshap(res(numRows^2+1:end,:),3*numRows, numCols);
-    p = p+rho*(w-x);
-    q = q+rho*(v-z);
+    
+    wk = reshape(res(1:numRows,:),numRows,numCols);
+    vk = reshape(res(numRows+1:end,:),3*numRows, numCols);
+
+    % Perform update
+    pk = pk + rho*(wk - xk); % n x n matrix
+    qk = qk + rho*(vk - zk); % 3n x n matrix
 end
-xSol = boxProx(p,t);
+xSol = boxProx(pk,t);
 end
