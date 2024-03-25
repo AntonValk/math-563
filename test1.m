cb52c1fd06b288485703687fe3ca3b1939970325
AGerkis <a.gerkis1@gmail.com>
2024-03-25 17:38:50 -0400

    Updated to also print test image, and to pass kernel to the algorithms.

--- test1.m
+++ test1.m
@@ -1,3 +1,5 @@
+clear; clc; close all;
+
 I = imread('cameraman.jpg');
 I = rgb2gray(I);   %black and white
 I = double(I(:, :, 1));
@@ -7,10 +9,15 @@ mx = max(I(:));
 I = I/mx;
 figure('Name','image before blurring')
 imshow(I,[])
-kernel = fspecial('gaussian', [15,15], 5);
+kernel = fspecial('gaussian', [5,5], 5);
 b = imfilter(I,kernel);
-noiseDensity=0.5;
+noiseDensity=0.25;
 b = imnoise(b,'salt & pepper',noiseDensity); 
 figure('Name','image after blurring')
 imshow(b,[]);
-Primal_DouglasRachford_Splitting(b, 1, 0.5);
+
+%im_clean = Primal_DouglasRachford_Splitting(b, kernel, 0.01, 1);
+im_clean = PrimalDual_DouglasRachford_Splitting(b, kernel, 0.01, 1);
+
+figure('Name', 'Image after de-blurring');
+imshow(im_clean, []);
