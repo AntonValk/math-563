c48b6ab52d6335c189e7dbd22dc1bd22b6cf5fbb
AGerkis <a.gerkis1@gmail.com>
2024-03-25 17:37:33 -0400

    Fixed matrix sizes so all lines are compatible and code runs.

--- Primal_DouglasRachford_Splitting.m
+++ Primal_DouglasRachford_Splitting.m
@@ -1,12 +1,12 @@
-function xSol = Primal_DouglasRachford_Splitting(b, t, rho)
+function xSol = Primal_DouglasRachford_Splitting(b, kernel, t, rho)
 [numRows, numCols]=size(b);
-z1 = zeros(numRows, numCols);   %z1 is for boxProx
-z21 = zeros(numRows, numCols);  % z21 is for l1/l2^2norm
-z22 = zeros(numRows, numCols);  %z22 and z23 are for isoNorm
-z23 = zeros(numRows, numCols);
+% z1 = zeros(numRows, numCols);   %z1 is for boxProx
+% z21 = zeros(numRows, numCols);  % z21 is for l1/l2^2norm
+% z22 = zeros(numRows, numCols);  %z22 and z23 are for isoNorm
+% z23 = zeros(numRows, numCols);
+
 %computes the numRow x numCol matrix of the eigenvalues for K and D1 and
 %D2; Here D1 = I oplus D1 in the paper and D2 = D1 oplus I.
-kernel = fspecial('gaussian', [15,15], 5);
 eigArry_K = eigValsForPeriodicConvOp(kernel, numRows, numCols);
 eigArry_D1 = eigValsForPeriodicConvOp([-1,1]', numRows, numCols);
 eigArry_D2 = eigValsForPeriodicConvOp([-1,1], numRows, numCols);
@@ -36,27 +36,39 @@ applyDTrans = @(y) applyD1Trans(y(:,:,1)) + applyD2Trans(y(:, :, 2));
 % Function which computes the (I + K^TK + D^TD)x where x in R^(m x n)
 % matrix and the eigenvalues of I + t*t*K^TK + t*t*D^TD; here t is the
 % stepsizes
-t=2.0    %t = i.tprimaldr; % Need to change this for the various algorithms you are applying
 applyMat = @(x) x + applyKTrans(applyK(x)) + applyDTrans(applyD(x));
 eigValsMat = ones(numRows, numCols) + t*t*eigArry_KTrans.*eigArry_K + t*t*eigArry_D1Trans.*eigArry_D1...
     + t*t*eigArry_D2Trans.*eigArry_D2;
 
 %R^(m x n) Computing (I + K^T*K + D^T*D)^(-1)*x
 invertMatrix = @(x) ifft2(fft2(x)./eigValsMat);
-for k = 1:500
-    w=zeros(numRows,numCols,2);
-    w(:,:,1)=z22;   % put z22 and z23 into a 3d matrix of size nxnx2
-    w(:,:,2)=z23;
-    x=boxProx(z1);
-    y1=l1Prox(z21); %y1 is a matrix of size nxn from l1Prox
-    y2=isoProx(w);
-    aux = 2*x(:) - z1(:)+(applyKTrans(2*y1(:)-z21(:))+applyDTrans(2*y2(:)-[z22;z23])); %x(:) and z(:) is to vectorize nxn matrix to a n^2 by 1 vector
-    u=invertMatrix(aux);
-    v=[applyK(u(1:numRows,:));applyD1(u(numRows+1:2*numRows,:));applyD2(u(2*numRows+1:end,:))];
-    z1=z1+rho*(u-x);
-    z21=z21+rho*(v(1:numRows,:)-y1);
-    z22=z22+rho*(v(numRows+1:2*numRows,:)-y2(1:numRows,:));
-    z23=z23+rho*(v(2*numRows+1:end,:)-y2(2*numRows+1:end,:));
+
+% Initialize
+%z1 = b;
+z1 = zeros(numRows, numCols);
+z2 = [applyK(z1); applyD1(z1); applyD2(z1)];
+
+for k = 1:5 %%TODO: Better stopping condition lol <- The paper suggests a stopping condition on the norm of the resolvents acting on x (pg5)
+    % Split z2 up
+    z21 = z2(1:numRows, :);
+    z22 = cat(3, z2((numRows + 1):2*numRows, :), z2((2*numRows + 1):end, :)); % Change z2 from 2D (2n*n)->3D (n*n*2)
+    
+    % Compute prox ops
+    x = boxProx(z1);
+    y1 = l1Prox(z21 - b, t) + b; % From Equation 8 in the reference
+    y2 = isoProx(z22, t);
+
+    % Compute resolvent of B
+    appK = applyKTrans(2*y1 - z21); % Compute the K component of the multiplication
+    appD = applyDTrans(2*y2 - z22); % Compute the D component of the multiplication (applyDTrans uses the concatenated form of the arrays)
+    u = invertMatrix(2*x - z1 + appK + appD); % Compute the resolvent
+    
+    % Compute Updates
+    v = [applyK(u); applyD1(u); applyD2(u)];
+    y = [y1; y2(:, :, 1); y2(:, :, 2)]; % Need to build y as a 2d matrix
+    
+    z1 = z1 + rho*(u - x);
+    z2 = z2 + rho*(v - y);
 end
-xSol=boxProx([z1;z21;z22;z23]);
+xSol=boxProx(z1);
 end 
