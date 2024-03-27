557f63d1c45cb78259e99100ba000fe8f25a1529
AGerkis <a.gerkis1@gmail.com>
2024-03-27 14:10:22 -0400

    Improved plot outputs for naive parameter sweep.

--- test1.m
+++ test1.m
@@ -18,21 +18,40 @@ imshow(b,[]);
 
 %im_clean = Primal_DouglasRachford_Splitting(b, kernel, 1, 0.5, 0.05, 250);
 im_clean = PrimalDual_DouglasRachford_Splitting(b, kernel, 0.5, 0.25, 0.01, 50);
+%im_clean = chambolle_pock(b, kernel, 1, 1, 0.05, 15);
+
 figure('Name', "Image after de-blurring");
 imshow(im_clean, []);
 
 %% Super Naive Parameter Sweep
-%step_sizes = [0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2.0];
-% step_sizes = [0.5, 1.0, 1.5];
-% reg_const = [0.1, 0.25, 0.5];
-% gammas = [0.01, 0.025, 0.05];
-% 
-% for i=1:length(step_sizes)
-%     for j=1:length(reg_const)
-%         for k=1:length(gammas)
-%             im_clean = PrimalDual_DouglasRachford_Splitting(b, kernel, step_sizes(i), reg_const(j), gammas(k), 50);
-%             figure('Name', "Image after de-blurring "+ num2str(step_sizes(i)) + ": " + num2str(reg_const(j))  + ": " + num2str(gammas(k)));
-%             imshow(im_clean, []);
-%         end
-%     end
-% end
+step_sizes = [0.01, 0.025, 0.04, 0.05, 0.1];
+reg_const = [0.1, 0.25, 0.5, 1.5];
+ss = [0.001, 0.025, 0.5, 1, 10]; %, 0.04, 0.05, 0.1];
+ts = ss; 
+gammas = [0.25, 0.5]; %[0.001, 0.0025, 0.005];
+
+for i=1:length(reg_const) %length(ss)
+    % Initialize plots for reg_const(i)
+    figure('Name', "Image after de-blurring - Regularization: " + num2str(reg_const(i)));
+    t = tiledlayout(length(step_sizes), length(gammas));
+
+    for j=1:length(step_sizes) %length(ts)
+        for k=1:length(gammas)
+            % De-blur image
+            im_clean = PrimalDual_DouglasRachford_Splitting(b, kernel, step_sizes(j), reg_const(i), gammas(k), 5);
+            %im_clean = chambolle_pock(b, kernel, ss(i), ts(j), gammas(k), 25);
+            %figure('Name', "Image after de-blurring "+ num2str(ss(i)) + ": " + num2str(ts(j))  + ": " + num2str(gammas(k)));
+            
+            nexttile; % Plot deblurred image
+            imshow(im_clean, []);
+
+            if j==1 % Add plot titles to top of layout
+                title("\gamma = " + num2str(gammas(k)),'FontWeight','Normal');
+            end
+
+            if k==1 % Add plot titles to left side of layout
+                ylabel("t = " + num2str(step_sizes(j)));
+            end
+        end
+    end
+end
