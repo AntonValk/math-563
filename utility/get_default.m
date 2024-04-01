9d73433b6dfa6ad2b836a12becf70124e8f015ea
AGerkis <a.gerkis1@gmail.com>
2024-04-01 09:20:27 -0400

    Wrote a function to output default parameter values.

---
+++ utility/get_default.m
@@ -0,0 +1,47 @@
+% get_default.m
+%
+% Returns the default parameter settings for the specified algorithm.
+%
+% Inputs:
+%   alg: The algorithm for which to return the default parameters. [String]
+%
+% Outputs:
+%   p: The parameter structure
+%
+% Author: Aidan Gerkis
+% Date: 31-3-2024
+
+function p = get_default(alg)
+    p = struct(); % Initialize
+    
+    % Output Parameters
+    p.verbose = 0; % Verbose mode disabled
+    p.display = 1; % Display plots of convergence
+    p.save_iters = 0; % Disable saving of all iterates
+
+    % Add global algorithm parameters
+    p.max_iter = 500; % Maximum number of iterations
+    p.e_t = 0.1; % Error threshold
+    p.e_meas = 'delta'; % Error metric to use when evaluating convergence
+    p.regularization = 'L2'; % Regularization type
+    
+    % Add algorithm specific parameters
+    switch alg 
+        case 'primal_dr'
+            p.t = 1; % Step size
+            p.gamma = 1.5; % Gamma
+            p.rho = 0.5; % Regularization constant
+        case 'primaldual_dr'
+            p.t = 1; % Step size
+            p.gamma = 1.5; % Gamma
+            p.rho = 0.5; % Regularization constant
+        case 'admm'
+            p.t = 1; % Step size
+            p.gamma = 1.5; % Gamma
+            p.rho = 0.5; % Regularization constant
+        case 'chambolle_pock'
+            p.t = 1; % Step size
+            p.s = 1; % Step size
+            p.gamma = 1.5; % Gamma
+    end
+end
