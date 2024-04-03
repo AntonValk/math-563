9f65008953087016a383a29712b19009722d0e4d
Antonios Valkanas <30447660+AntonValk@users.noreply.github.com>
2024-04-02 21:41:06 -0400

    Update mutual_info.m

--- utility/mutual_info.m
+++ utility/mutual_info.m
@@ -29,18 +29,6 @@
 
 
 function I=mutual_info(A,B,varargin) 
-%MI Determines the mutual information of two images or signals
-%
-%   I=mi(A,B)   Mutual information of A and B, using 256 bins for
-%   histograms
-%   I=mi(A,B,L) Mutual information of A and B, using L bins for histograms
-%
-%   Assumption: 0*log(0)=0
-%
-%   See also ENTROPY.
-%   jfd, 15-11-2006
-%        01-09-2009, added case of non-double images
-%        24-08-2011, speed improvements by Andrew Hill
 if nargin>=3
     L=varargin{1};
 else
